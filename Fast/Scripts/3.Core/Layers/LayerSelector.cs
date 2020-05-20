8f57229933eee9a193f3880a84ef64fbaff50493
guillemsunyer-omnidrone <guillem.sunyer@omnidrone.net>
2020-05-21 00:45:58 +0200

    Removed cinematics

---
+++ Fast/Scripts/3.Core/Layers/LayerSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fast.Layers
+{ 
+    public class LayerSelector<T> : MonoBehaviour where T : Enum
+    {
+        [SerializeField] private T layer = default;
+        [SerializeField] private Renderer target_renderer = null;
+
+        private void Awake()
+        {
+            SetLayer();
+        }
+
+        private int LayerToInt()
+        {
+            return Convert.ToInt32(layer);
+        }
+
+        private void SetLayer()
+        {
+            if(target_renderer == null)
+            {
+                return;
+            }
+
+            target_renderer.sortingOrder = LayerToInt();
+        }
+    }
+}
